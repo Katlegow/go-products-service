e7066e7d4379ca23a3a2fe59d74f10515f239503
Katlegow <mosweukatlego.km@gmail.com>
2022-02-25 14:54:25 +0200

    Added db interaction

--- model.go
+++ model.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 )
 
 type product struct {
@@ -12,17 +11,47 @@ type product struct {
 }
 
 func (prod *product) getProduct(db *sql.DB) error {
-	return errors.New("Not implemented yet!")
+	return db.QueryRow("SELECT FROM product WHERE id = $1", prod.ID).Scan(&prod.Name, &prod.Price)
 }
 
 func (prod *product) updateProduct(db *sql.DB) error {
-	return errors.New("Not implemented yet!")
+	_, err := db.Exec("UPDATE product SET name = $1, price = $2 WHERE id = $3",
+		prod.Name,
+		prod.Price,
+		prod.ID)
+
+	return err
 }
 
 func (prod *product) createProduct(db *sql.DB) error {
-	return errors.New("Not implemented yet!")
+
+	return db.QueryRow("INSERT INTO product(name, price) VALUES($1, $2) RETURNING id",
+		prod.Name,
+		prod.Price).Scan(&prod.ID)
 }
 
 func getAllProducts(db *sql.DB, start, count int) ([]product, error) {
-	return nil, errors.New("Not implemented yet!")
+
+	rows, err := db.Query("SELECT * FROM product LIMIT $1 OFFSET $2", count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	products := []product{}
+
+	for rows.Next() {
+
+		var p product
+
+		if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+			return nil, err
+		}
+
+		products = append(products, p)
+	}
+
+	return products, nil
 }
